c49b26e32d20f4ff9bbfe13371a4d9be7ebbccf6
Charlotte Gore <conspiracygore@gmail.com>
2013-11-07 17:11:03 +0000

    wip

--- index.js
+++ index.js
@@ -2,37 +2,43 @@ var View = require('hyperbone-view').HyperboneView;
 var Model = require('hyperbone-model').Model;
 var dom = require('dom');
 
+/*
+ * Add some extensions to hyperbone view. First pluralise.
+ */
+
 require('hyperbone-view').registerHelper('pluralise', function( attr, str ){
 	return (attr==1 ? str : str + "s");
 });
 
+/*
+ * Then 'if'. Would you believe it there isn't an 'if' directive in hyperbone view yet.
+ */
+
 require('hyperbone-view').registerAttributeHandler('if', function( node, prop, cancel ){
 
-	var self = this;
-
-	var test = function(){
-		if(self.model.get(prop)){
-			dom(node).css({display: ''})			
-		}else{
-			dom(node).css({display: 'none'});
-		}
-	}
+	var self = this, 
+		test = function(){
+			dom(node).css({display: ( self.model.get(prop)!==0 ? '': 'none') });	
+		};
 
 	this.model.on('change:' + prop, function(){
-
+		debugger;
 		test();
 
 	});
 
 	test();
 
-})
+});
 
-var html = dom('#todoapp');
+/*
+ * Create a model with one default item already in it.
+ */
 
-var model = new Model({
+window.model = new Model({
 	remaining : 0,
-	completed : 0,
+	completed : 1,
+	"new-item" : "",
 	items : [
 	{
 		completed : "completed",
@@ -41,26 +47,58 @@ var model = new Model({
 	]
 });
 
+/*
+ * Bind to collection add/remove/change events to update calulations
+ */
+
+model.on('change:items', function(){
+
+	// turns out we don't get a generic 'change' event when a collection has been changed. BUG!
+	// this callback never fires...
+	model.set('remaining', model
+							.get('items')
+							.select(function(el){ 
+								return !el.get('completed')
+							})
+							.length);
+
+	model.set('completed', model
+							.get('items')
+							.select(function(el){ 
+								return el.get('completed')
+							})
+							.length);
+});
+
+model.on('change:new-item', function(){
+
+	var val = model.get('new-item');
+
+	if(val!=""){
+		model.get('items').add({
+			completed : "",
+			task : model.get('new-item')
+		});
+		model.set('new-item', '');
+	}
+
+});
+
 new View({
-	model : model,
-	el : html.els[0],
-	delegates : {
+	model : model, // our model
+	el : dom('#todoapp'), // a reference to our application root.
+	delegates : { // delegates to capture 
 		'keypress #new-todo' : function(e){
 			if(e.which===13){
-				model.get('items').add({
-					completed : "",
-					task : dom('#new-todo').val()
-				});
 
-				dom('#new-todo').val("");
 			}
+		},
+		'click #clear-completed' : function(e){
+			// this has exposed a bug in the collection handling stuff. If you try to set an existing collection
+			// with an array of models... it's maximum call stack time. Shittington.
+			var remaining = model.get('items').select(function(el){ return !el.get('completed'); })
+			model.get('items').remove(remaining);
 		}
 	}
 });
 
-model.get('items').on('change add remove', function(){
-
-	model.set('remaining', model.get('items').select(function(el){ return !el.get('completed')}).length);
-	model.set('completed', model.get('items').select(function(el){ return el.get('completed')}).length);
-
-});
